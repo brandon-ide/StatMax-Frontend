3e30f413d5e1656607f71895587057327836e575
Brandon Ide <brandon.d.ide@gmail.com>
2025-11-28 19:17:00 -0500

    MobileHeightJSAdded

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from './App';
 import './styles/main.css';
 import { AuthProvider } from './context/AuthContext';
 
+const setVh = () => {
+  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+};
+setVh();
+window.addEventListener('resize', setVh);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
